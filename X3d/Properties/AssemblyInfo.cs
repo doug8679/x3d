acd2cdf0be05a55cd7a4184b943745d919fc7716
Alexander Dong Back Kim <kimdba@hotmail.com>
2012-11-07 12:31:47 -0800

    Adding X3d project into the solution

---
+++ X3d/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="X3D.NET">
+//   X3D Package Assembly Information
+// </copyright>
+// <summary>
+//   
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("X3D.NET Library")]
+[assembly: AssemblyDescription("X3D.NET Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://x3d.codeplex.com/")]
+[assembly: AssemblyProduct("X3D.NET Library")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("X3D.NET")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("40f0ff62-bf60-420e-9ff8-96ae1d28d3dd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
