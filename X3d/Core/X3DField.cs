422fcf7ebdc78524a62192cb6c9c4c44117bfeb7
Alexander Dong Back Kim <kimdba@hotmail.com>
2012-11-07 12:51:38 -0800

    Adding component directories to the solution file. From now on there will be a dedicated development branch for each component. For example, if I'm working on Rendering component then I will switch my workspace to Rendering branch. Moreover, there will be release branches. Each release branch will ideally have latest stable versions from each component development.

---
+++ X3d/Core/X3DField.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="X3DField.cs" company="X3D.NET">
+//   X3D.NET Library
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace X3d.Core
+{
+    /// <summary>
+    /// X3DField is the abstract field type from which all single values field types are derived.
+    /// </summary>
+    public abstract class X3DField
+    {
+    }
+}
