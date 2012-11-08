fac594b0dad706639ff5ea2ce906f4fac59f73ab
Alexander Dong Back Kim <kimdba@hotmail.com>
2012-11-08 09:39:45 -0800

    Implementing SFBool

---
+++ X3d/Core/SFBool.cs
@@ -0,0 +1,11 @@
+﻿namespace X3d.Core
+{
+    /// <summary>
+    /// The SFBool field specifies a single Boolean value. Each Boolean value 
+    /// represents either TRUE or FALSE. How these values are represented is 
+    /// encoding dependent.
+    /// </summary>
+    public class SFBool
+    {
+    }
+}
---
+++ X3dTest/Core/TestSFBool.cs
@@ -0,0 +1,14 @@
+﻿namespace X3dTest.Core
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using X3d.Core;
+
+    /// <summary>
+    ///   <see cref="X3d.Core.SFBool" /> unit test cases
+    /// </summary>
+    [TestClass]
+    public class TestSFBool
+    {
+    }
+}
