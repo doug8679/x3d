d827d06ac5e4cb753d42c86697ee17e8815379c6
Alexander Dong Back Kim <kimdba@hotmail.com>
2012-11-08 09:38:40 -0800

    Started working on Core component.

--- X3d/Core/X3DField.cs
+++ X3d/Core/X3DField.cs
@@ -1,13 +1,8 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="X3DField.cs" company="X3D.NET">
-//   X3D.NET Library
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-namespace X3d.Core
+﻿namespace X3d.Core
 {
     /// <summary>
-    /// X3DField is the abstract field type from which all single values field types are derived.
+    /// X3DField is the abstract field type from which all single values field 
+    /// types are derived.
     /// </summary>
     public abstract class X3DField
     {
