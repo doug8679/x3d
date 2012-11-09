2f866830f3b37a6ff8e7ab852f0bdb0780af7145
Alexander Dong Back Kim <kimdba@hotmail.com>
2012-11-09 09:15:08 -0800

    SFString has been implemented and tested.

---
+++ X3d/Core/SFString.cs
@@ -0,0 +1,76 @@
+﻿namespace X3d.Core
+{
+    /// <summary>
+    /// The SFString field contain strings encoded with the UTF-8 universal 
+    /// character set. Any characters (including linefeeds and '#') may appear 
+    /// within the string. The default value of an uninitialized SFString 
+    /// outputOnly field is the empty string. 
+    /// </summary>
+    public class SFString : X3DField
+    {
+        #region Constructors
+
+        public SFString()
+        {
+            this.Data = string.Empty;
+        }
+
+        public SFString(string data)
+        {
+            this.Data = data;
+        }
+
+        public SFString(SFString obj)
+        {
+            this.Data = obj.Data;
+        }
+
+        #endregion
+
+        private string Data { get; set; }
+
+        #region Object Equality
+
+        public override int GetHashCode()
+        {
+            return this.Data.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is SFString)
+            {
+                return this.Data.Equals(((SFString)obj).Data);
+            }
+            else if (obj is string)
+            {
+                return this.Data.Equals(obj);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region String Compatibility
+
+        public override string ToString()
+        {
+            return this.Data;
+        }
+        
+        public static implicit operator SFString(string obj)
+        {
+            return new SFString(obj);    
+        }
+
+        public static implicit operator string(SFString obj)
+        {
+            return obj.Data;
+        }
+
+        #endregion
+    }
+}
---
+++ X3dTest/Core/TestSFString.cs
@@ -0,0 +1,70 @@
+﻿namespace X3dTest.Core
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using X3d.Core;
+
+    /// <summary>
+    /// <see cref="X3d.Core.SFString"/> unit test cases
+    /// </summary>
+    [TestClass]
+    public class TestSFString
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            SFString a = "hello";
+            var b = new SFString();
+            var c = new SFString("hello");
+            var d = new SFString(c);
+            
+            Assert.AreEqual(a, c);
+            Assert.AreEqual(a, d);
+            Assert.AreNotEqual(a, b);
+        }
+
+        [TestMethod]
+        public void StringCompatibility()
+        {
+            SFString a = "X3D";
+            var b = a;
+            SFString c = b;
+
+            Assert.IsTrue(a == b);
+            Assert.IsTrue(b == c);
+            Assert.IsTrue(c == a);
+        }
+
+        [TestMethod]
+        public void Equality()
+        {
+            SFString a = "test";
+            int b = 0;
+
+            Assert.AreEqual(a, a);
+            Assert.AreNotEqual(a, b);
+        }
+
+        [TestMethod]
+        public void StringConstruction()
+        {
+            SFString a = "hello";
+            string b = a;
+
+            Assert.AreEqual(a.ToString(), b);
+            Assert.AreEqual(a, a.ToString());
+        }
+
+        [TestMethod]
+        public void HashCode()
+        {
+            SFString a = "hello";
+            SFString b = "hello";
+            string c = "hello";
+
+            Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+            Assert.AreEqual(a.GetHashCode(), c.GetHashCode());
+            Assert.AreEqual(b.GetHashCode(), c.GetHashCode());
+        }
+    }
+}
