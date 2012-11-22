660fb53199aa3f700329df46b237469537471de4
Alexander Dong Back Kim <kimdba@hotmail.com>
2012-11-22 00:35:59 -0800

    SFNode completed

---
+++ X3dTest/Core/TestSFNode.cs
@@ -0,0 +1,27 @@
+﻿namespace X3dTest.Core
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using X3d.Core;
+
+    [TestClass]
+    public class TestSFNode
+    {
+        [TestMethod]
+        public void Constructors()
+        {
+            var node = new SFNode();
+
+            Assert.AreEqual(node.ToString(), "NULL");
+
+            node = new SFNode("foo");
+
+            Assert.AreEqual(node.ToString(), "foo");
+
+            node = new SFNode(new SFNode("bar"));
+
+            Assert.AreEqual(node.ToString(), "bar");
+        }
+    }
+}
