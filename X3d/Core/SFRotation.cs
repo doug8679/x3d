4c062253add3240df4dd15f3fa2f7700e4472bd1
Alexander Dong Back Kim <kimdba@hotmail.com>
2012-11-22 19:23:09 -0800

    SFRotation is completed. Now all single field types are implemented.

---
+++ X3d/Core/SFRotation.cs
@@ -0,0 +1,65 @@
+﻿namespace X3d.Core
+{
+    /// <summary>
+    /// The SFRotation field specifies one arbitrary rotation. An SFRotation is
+    /// written to the X3D file as four floating point values. The allowable 
+    /// form for a floating point number is defined in the specific encoding. 
+    /// The first three values specify a normalized rotation axis vector about 
+    /// which the rotation takes place. The fourth value specifies the amount 
+    /// of right-handed rotation about that axis in radians.
+    /// </summary>
+    public class SFRotation : SFVec3f
+    {
+        #region Constructors
+
+        public SFRotation()
+            : this(0, 0, 1, 0)
+        {
+        }
+
+        public SFRotation(SFFloat x, SFFloat y, SFFloat z, SFFloat angel)
+            : base(x, y, z)
+        {
+            this.Angle = new SFFloat(angel);
+        }
+
+        #endregion
+
+        public SFFloat Angle { get; set; }
+
+        #region Object Equality
+
+        public override int GetHashCode()
+        {
+            var angel = this.Angle.GetHashCode() * HashTablePrimeNumbers[3];
+
+            return base.GetHashCode() ^ angel;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is SFRotation)
+            {
+                var casted = (SFRotation)obj;
+
+                return this.X.Equals(casted.X)
+                    && this.Y.Equals(casted.Y)
+                    && this.Z.Equals(casted.Z)
+                    && this.Angle.Equals(casted.Angle);
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region String Compatibility
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3}", this.X, this.Y, this.Z, this.Angle);
+        }
+
+        #endregion
+    }
+}
