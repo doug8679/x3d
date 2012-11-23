26816af8c3469ca355a4584042d0d44295802df8
Alexander Dong Back Kim <kimdba@hotmail.com>
2012-11-23 03:41:08 -0800

    X3D nodes in the core components are prepared. Need to implement few more thing.

---
+++ X3d/Core/X3DChildNode.cs
@@ -0,0 +1,11 @@
+﻿namespace X3d.Core
+{
+    /// <summary>
+    /// This abstract node type indicates that the concrete nodes that are 
+    /// instantiated based on it may be used in children, addChildren, and 
+    /// removeChildren fields.
+    /// </summary>
+    public abstract class X3DChildNode : X3DNode
+    {
+    }
+}
---
+++ X3d/Core/X3DInfoNode.cs
@@ -0,0 +1,10 @@
+﻿namespace X3d.Core
+{
+    /// <summary>
+    /// This is the base node type for all nodes that contain only information 
+    /// without visual semantics.
+    /// </summary>
+    public abstract class X3DInfoNode : X3DChildNode
+    {
+    }
+}
