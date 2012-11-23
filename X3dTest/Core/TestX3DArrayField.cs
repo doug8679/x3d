8760ce786d9e0a83cf9e22febeff06956476ac9f
Alexander Dong Back Kim <kimdba@hotmail.com>
2012-11-22 19:54:53 -0800

    Field type implementation is completed.

---
+++ X3dTest/Core/TestX3DArrayField.cs
@@ -0,0 +1,22 @@
+﻿namespace X3dTest.Core
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using X3d.Core;
+
+    [TestClass]
+    public class TestX3DArrayField
+    {
+        [TestMethod]
+        public void StringCompatibility()
+        {
+            var a = new MFBool();
+
+            a.Add(false);
+            a.Add(true);
+
+            var str = a.ToString();
+
+            Assert.AreEqual(str, "False, True");
+        }
+    }
+}
