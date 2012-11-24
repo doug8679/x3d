a8b50286fcfbf769d59ebdbc7fd6ce44ac43b22f
Alexander Dong Back Kim <kimdba@hotmail.com>
2012-11-24 00:44:19 -0800

    Added few test cases against core component

---
+++ X3dTest/Core/TestComponent.cs
@@ -0,0 +1,52 @@
+﻿namespace X3dTest.Core
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using X3d.Core;
+
+    /// <summary>
+    /// <see cref="X3d.Core.Component" /> unit test cases
+    /// </summary>
+    [TestClass]
+    public class TestComponent
+    {
+        [TestMethod]
+        public void Level()
+        {
+            var obj = new Component();
+
+            obj.Level = 1;
+            Assert.AreEqual(obj.Level, 1);
+
+            obj.Level = 2;
+            Assert.AreEqual(obj.Level, 2);
+
+            obj.Level = 3;
+            Assert.AreEqual(obj.Level, 3);
+
+            obj.Level = 4;
+            Assert.AreEqual(obj.Level, 4);
+
+            obj.Level = 5;
+            Assert.AreEqual(obj.Level, 5);
+
+            try
+            {
+                obj.Level = 0;
+                Assert.Fail();
+            }
+            catch (ArgumentException)
+            {
+            }
+
+            try
+            {
+                obj.Level = 6;
+                Assert.Fail();
+            }
+            catch (ArgumentException)
+            {
+            }
+        }
+    }
+}
