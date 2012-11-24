9dd4ffe9e4dc3926590f921e4d049ced1bcdc14f
Alexander Dong Back Kim <kimdba@hotmail.com>
2012-11-23 18:20:05 -0800

    Starts testing XML serialization

---
+++ X3dTest/Core/TestXMLSerialization.cs
@@ -0,0 +1,13 @@
+﻿namespace X3dTest.Core
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestXMLSerialization
+    {
+        //[TestMethod]
+        //public void Constructors()
+        //{
+        //}
+    }
+}
